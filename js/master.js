521b8962d6f8bf3737323100c23eef00d949fdbf
federico d'alessio <federico1993.d.alessio.1993@gmail.com>
2021-04-06 12:38:40 +0200

    prima milestone

--- js/master.js
+++ js/master.js
@@ -4,4 +4,29 @@ var root = new Vue({
 
     el : "#root",
 
+    data : {
+        movieList : [],
+        srcParams : ""
+    },
+
+    methods : {
+        srcEvent : function()
+                    {
+                        axios
+                        .get("https://api.themoviedb.org/3/search/movie",
+                                {
+                                    params: {
+                                    api_key : "255bb4b57c925de984771ce579e8090f",
+                                    query : this.srcParams,
+                                    language : "it"
+                                }
+                            })
+                        .then((r) =>
+                            {
+                                this.movieList = r.data.results;
+                                console.log(this.movieList);
+                            })
+                    }
+    }
+
 })
