2e1551df9966814a9fbdbd63bfe58cd3f585519c
federico d'alessio <federico1993.d.alessio.1993@gmail.com>
2021-04-07 16:31:44 +0200

    milestone 2. Aggiunta stelle

--- js/master.js
+++ js/master.js
@@ -5,8 +5,10 @@ var root = new Vue({
     el : "#root",
 
     data : {
+        films: [],
+        tvSeries : [],
         movieList : [],
-        srcParams : ""
+        srcParams : "",
     },
 
     methods : {
@@ -23,9 +25,46 @@ var root = new Vue({
                             })
                         .then((r) =>
                             {
-                                this.movieList = r.data.results;
-                                console.log(this.movieList);
+                                this.films= r.data.results;
+                                // this.movieList = this.movieList.concat(this.films)
+                                console.log(this.movieList)
+                                this.movieList.forEach(e => 
+                                {   
+                                    let tmp = e.vote_average
+                                    e.vote_average = []
+                                    votes = Math.ceil(tmp/2);
+                                    for(let i=0; i<5; i++)
+                                    {
+                                        if(i < votes)
+                                        {
+                                            e.vote_average.push("fas fa-star")
+                                        }
+                                        else
+                                        {
+                                            e.vote_average.push("far fa-star")
+                                        }
+                                        
+                                    }
+                                });
                             })
+                        
+                        axios
+                        .get("https://api.themoviedb.org/3/search/tv",
+                                {
+                                    params: {
+                                    api_key : "255bb4b57c925de984771ce579e8090f",
+                                    query : this.srcParams,
+                                    language : "it"
+                                }
+                            })
+                        .then((ris)=>
+                        {
+                            this.tvSeries = ris.data.results
+                            
+                        })
+                        this.movieList = this.movieList.concat(this.tvSeries, this.films)
+                        console.log(this.movieList)
+                        // this.movieList = [];
                     }
     }
 
