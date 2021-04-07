8efc9ae09054751f7df0b89844ed43e881b363aa
federico d'alessio <federico1993.d.alessio.1993@gmail.com>
2021-04-07 18:01:17 +0200

    aggiunta lista serie tv,

--- js/master.js
+++ js/master.js
@@ -4,16 +4,20 @@ var root = new Vue({
 
     el : "#root",
 
-    data : {
-        films: [],
+    data : 
+    {
         tvSeries : [],
         movieList : [],
+        films : [],
         srcParams : "",
     },
 
     methods : {
         srcEvent : function()
-                    {
+                    {   
+                        this.movieList = [];
+                        this.tvSeries = [];
+                        this.films = [];
                         axios
                         .get("https://api.themoviedb.org/3/search/movie",
                                 {
@@ -25,46 +29,48 @@ var root = new Vue({
                             })
                         .then((r) =>
                             {
-                                this.films= r.data.results;
-                                // this.movieList = this.movieList.concat(this.films)
-                                console.log(this.movieList)
-                                this.movieList.forEach(e => 
-                                {   
-                                    let tmp = e.vote_average
-                                    e.vote_average = []
-                                    votes = Math.ceil(tmp/2);
-                                    for(let i=0; i<5; i++)
+                                var tempMovies = r.data.results;
+                                console.log(this.films)
+
+                                axios
+                                .get("https://api.themoviedb.org/3/search/tv",
                                     {
-                                        if(i < votes)
-                                        {
-                                            e.vote_average.push("fas fa-star")
-                                        }
-                                        else
-                                        {
-                                            e.vote_average.push("far fa-star")
-                                        }
-                                        
+                                        params: {
+                                        api_key : "255bb4b57c925de984771ce579e8090f",
+                                        query : this.srcParams,
+                                        language : "it"
                                     }
-                                });
+                                })
+                                .then((ris)=>
+                                    {
+                                        console.log("chiamata")
+                                        this.tvSeries = ris.data.results;
+                                        console.log(this.tvSeries);
+                                        tempMovies = tempMovies.concat(this.tvSeries);
+                                        tempMovies.forEach(e => 
+                                            {   
+                                                console.log("test");
+                                                let tmp = e.vote_average
+                                                e.vote_average = [];
+                                                votes = Math.ceil(tmp/2);
+                                                for(let i=0; i<5; i++)
+                                                {
+                                                    if(i < votes)
+                                                    {
+                                                        e.vote_average.push("fas fa-star")
+                                                    }
+                                                    else
+                                                    {
+                                                        e.vote_average.push("far fa-star")
+                                                    }
+                                                    
+                                                }
+                                            });
+                                        this.movieList = tempMovies;
+                                        console.log("concat", this.movieList);
+                                    })
                             })
                         
-                        axios
-                        .get("https://api.themoviedb.org/3/search/tv",
-                                {
-                                    params: {
-                                    api_key : "255bb4b57c925de984771ce579e8090f",
-                                    query : this.srcParams,
-                                    language : "it"
-                                }
-                            })
-                        .then((ris)=>
-                        {
-                            this.tvSeries = ris.data.results
-                            
-                        })
-                        this.movieList = this.movieList.concat(this.tvSeries, this.films)
-                        console.log(this.movieList)
-                        // this.movieList = [];
                     }
     }
 
